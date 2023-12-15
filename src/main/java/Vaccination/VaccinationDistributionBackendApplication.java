c6326d68106d064bbe3cc72ee75cfe89264ea7bf
AnujKaprawan <anujkp15@gmail.com>
2023-12-15 19:03:30 +0530

    Basic frame added

---
+++ src/main/java/Vaccination/VaccinationDistributionBackendApplication.java
@@ -0,0 +1,13 @@
+package Vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationDistributionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationDistributionBackendApplication.class, args);
+	}
+
+}
